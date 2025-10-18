ba8b00cf17b3447dfefc32feedf69cb1bf7b77d7
phwinter <p.winter@campus.lmu.de>
2025-10-18 10:46:54 +0200

    Notizen zur ersten VL

---
+++ NotizenBefehle.R
@@ -0,0 +1,10 @@
+ 
+# i: name bearbeiten; esc um raus zu kommen 
+#w: speichern
+#q: beenden
+
+
+#git branch, git merge, git status
+# git add "Rfile" , git commit (statt commit und speichern drücken)
+#Rückgängig machen: git revert "SHA vom ausgewählten Punkt im branch" eher nicht verwenden
+# git checkout
